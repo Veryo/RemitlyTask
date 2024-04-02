ff64723f104a3dc7381fcd501bda186c55b102ca
Jakub Wojcik <wcuba@wp.pl>
2024-04-02 16:11:59 +0200

    add missing validations

--- tests/test_load_policy_file.py
+++ tests/test_load_policy_file.py
@@ -9,28 +9,39 @@ from main.is_policy_resource_asterisk_free import load_policy_file
 
 class TestLoadPolicyFile(unittest.TestCase):
     
+
+    def test_missing_policy_name_key_error(self):
+        with self.assertRaises(ValueError):
+            load_policy_file("fixtures/missing_policy_name.json")
+
+    def test_policy_name_not_string_error(self):
+        with self.assertRaises(ValueError):
+            load_policy_file("fixtures/policy_name_not_string.json")
+
     def test_load_valid_policy_file(self):
-        """Test reading a valid policy file."""
         policy = load_policy_file("fixtures/valid_policy.json")
         self.assertIn("Statement", policy["PolicyDocument"])
 
 
     def test_file_not_found_error(self):
-        """Test the function raises FileNotFoundError for non-existent files."""
         with self.assertRaises(FileNotFoundError):
             load_policy_file("non_existent_file.json")
 
     def test_invalid_json_error(self):
-        """Test the function raises ValueError for files with invalid JSON."""
         with self.assertRaises(ValueError):
             load_policy_file("fixtures/invalid_json_policy.json")
 
     def test_invalid_policy_format_error(self):
-        """Test the function raises ValueError for valid JSON that doesn't match the policy format."""
         with self.assertRaises(ValueError):
             load_policy_file("fixtures/invalid_format_policy.json")
 
+    def test_missing_policy_document_key_error(self):
+        with self.assertRaises(ValueError):
+            load_policy_file("fixtures/missing_policy_document.json")
+
     def test_no_statement_key_error(self):
-        """Test the function raises ValueError for JSON objects without 'Statement'."""
         with self.assertRaises(ValueError):
             load_policy_file("fixtures/no_statement_key.json")
+
+    
+  
